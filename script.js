98f5efe2322180cea2d2a990711137e026815e53
Arggonaut <and123zhe@gmail.com>
2026-03-25 00:06:54 -0400

    reimplemented the image to ascii logic

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let scale_input = document.getElementById("scale_input");
 let scale = scale_input.valueAsNumber;
 let convert_button = document.getElementById("convert_button");
 let resized_image = document.getElementById("resized_image");
+let ascii_canvas = document.getElementById("ascii_canvas");
+let ascii_text_area = document.getElementById("ascii_output");
 
 let height_to_width_ratio = 1;
 
@@ -36,11 +38,42 @@ scale_input.addEventListener("change", (event) => {
 
 convert_button.addEventListener("click", (event) => {
 
-    let width = 2 ** scale;
-    let height = width * height_to_width_ratio;
+    let resized_width = 2 ** scale;
+    let resized_height = resized_width * height_to_width_ratio / 2;
 
-    let resizedURL = resizeImage(input_image, width, height);
+    ascii_text_area.cols = resized_width;
+    ascii_text_area.rows = resized_height;
+
+    let resizedURL = resizeImage(input_image, resized_width, resized_height);
     resized_image.src = resizedURL;
+    
+    let ascii_canvas_context = ascii_canvas.getContext("2d"); 
+    ascii_canvas.width = resized_image.width; 
+    ascii_canvas.height = resized_image.height;
+    ascii_canvas_context.drawImage(resized_image, 0, 0, resized_image.width, resized_image.height); 
+    const imageData = ascii_canvas_context.getImageData(0, 0, resized_image.width, resized_image.height); //get imageData of the image
+
+    let pixel = imageData.data; //get an array of the pixel data
+    let ascii = ""; //declare the output string
+    for (let i = 0, column = 0, row = 1; i < pixel.length; i+=4) { //loop through each pixel
+        let brightness = parseInt((pixel[i] + pixel[i + 1] + pixel[i + 3]) / 3); //average the rgb to get the brightness
+        let densityIndex = parseInt(brightness / DENSITY_CHAR_RANGE); //find the index for the density char for the brightness level
+        ascii = ascii.concat(DENSITY_CHARS.charAt(densityIndex)); //concatinate the corrosponding density char to the ascii string
+
+
+        if (column == (resized_image.width - 1)) { //if we reached the end of the row of pixels
+            console.log(row);
+            row++;
+            ascii = ascii.concat("\n"); //concatinate a linebreak
+            column = 0; //set column to zero - start of new row
+        }
+        else {
+            column++; //move on to the next column of the row
+        }
+    }
+    ascii_text_area.textContent = ascii;
+
+
 })
 
    
@@ -52,28 +85,6 @@ convert_button.addEventListener("click", (event) => {
 
 
 
-
-//////
-// const imageData = context.getImageData(0, 0, IMAGE.width, IMAGE.height); //get imageData of the image
-// let pixel = imageData.data; //get an array of the pixel data
-// let ascii = ""; //declare the output string
-// for (let i = 0, column = 0, row = 1; i < pixel.length; i+=4) { //loop through each pixel
-//     let brightness = parseInt((pixel[i] + pixel[i + 1] + pixel[i + 3]) / 3); //average the rgb to get the brightness
-//     let densityIndex = parseInt(brightness / DENSITY_CHAR_RANGE); //find the index for the density char for the brightness level
-//     ascii = ascii.concat(DENSITY_CHARS.charAt(densityIndex)); //concatinate the corrosponding density char to the ascii string
-
-
-//     if (column == (IMAGE.width - 1)) { //if we reached the end of the row of pixels
-//         console.log(row);
-//         row++;
-//         ascii = ascii.concat("\n"); //concatinate a linebreak
-//         column = 0; //set column to zero - start of new row
-//     }
-//     else {
-//         column++; //move on to the next column of the row
-//     }
-// }
-// console.log(ascii); //output the ascii
 
 
 
